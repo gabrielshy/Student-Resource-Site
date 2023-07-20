5444461f9288566170732979655c620e6557e146
Francis Anos <73969533+gabrielshy@users.noreply.github.com>
2023-07-20 01:34:43 -0500

    Small Change
    Added python script

--- python/main.py
+++ python/main.py
@@ -1,14 +1,19 @@
 import requests
 
-url = "https://ebay-products-search-scraper.p.rapidapi.com/products"
+url = "https://price-analytics.p.rapidapi.com/search-by-term"
 
-querystring = {"query":"xbox","page":"1","Item_Location":"europe"}
-
+payload = {
+	"source": "amazon",
+	"country": "us",
+	"values": "College Textbook"
+}
 headers = {
+	"content-type": "application/x-www-form-urlencoded",
 	"X-RapidAPI-Key": "e50ad5c2damsh66bf684007f0937p16dc4cjsnbddd3dd25e62",
-	"X-RapidAPI-Host": "ebay-products-search-scraper.p.rapidapi.com"
+	"X-RapidAPI-Host": "price-analytics.p.rapidapi.com"
 }
 
-response = requests.get(url, headers=headers, params=querystring)
+response = requests.post(url, data=payload, headers=headers)
 
 print(response.json())
+
