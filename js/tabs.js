566ac42abadc2e4fbfdbf36fad5cfe1dfa7f2585
Francis Anos <73969533+gabrielshy@users.noreply.github.com>
2023-07-24 14:00:42 -0500

    Tabs Change

--- js/tabs.js
+++ js/tabs.js
@@ -1,18 +1,33 @@
-var tabButtons=document.querySelectorAll(".tabContent .buttonContainer button");
-var tabPanels=document.querySelectorAll(".tabContent .tabPanel");
+var tabButtons = document.querySelectorAll(".tabContent .buttonContainer button");
+var tabPanels = document.querySelectorAll(".tabContent .tabPanel");
 
-function showPanel(panelIndex,colorCode) {
-    tabButtons.forEach(function(node) {
-        node.style.backgroundColor="";
-        node.style.color="";
-      });
-      tabButtons[panelIndex].style.backgroundColor=colorCode;
-      tabButtons[panelIndex].style.color="background-color: var(--primary-color);";
-      tabPanels.forEach(function(node) {
-        node.style.display="none";
-
-      });
-      tabPanels[panelIndex].style.display="block";
-      tabPanels[panelIndex].style.backgroundColor=colorCode;
+function showPanel(panelIndex, colorCode) {
+    tabButtons.forEach(function (node, index) {
+        if (index === panelIndex) {
+            node.classList.add("active");
+            node.classList.remove("non-active");
+        } else {
+            node.classList.remove("active");
+            node.classList.add("non-active");
+        }
+        node.style.backgroundColor = "";
+        node.style.color = "";
+    });
+    tabButtons[panelIndex].style.backgroundColor = colorCode;
+    tabButtons[panelIndex].style.color = "background-color: var(--primary-color);";
+    tabPanels.forEach(function (node) {
+        node.style.display = "none";
+    });
+    tabPanels[panelIndex].style.display = "block";
+    tabPanels[panelIndex].style.backgroundColor = colorCode;
 }
-showPanel(0,'background-color: var(--primary-color);');
+
+// Attach click event listeners to each tab button
+tabButtons.forEach(function (button, index) {
+    button.addEventListener("click", function () {
+        showPanel(index, 'background-color: var(--primary-color);');
+    });
+});
+
+// Show the first tab by default
+showPanel(0, 'background-color: var(--primary-color);');
